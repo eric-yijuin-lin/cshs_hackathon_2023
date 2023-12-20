67eaa0b6cce675cdef62ec369b477482b6a23768
Eric Lin <eric.yijuin.lin@gmail.com>
2023-12-20 09:04:47 +0800

    show path when config loading is failed

--- utilities.py
+++ utilities.py
@@ -2,8 +2,7 @@ import json
 
 def read_config(file_path: str) -> dict:
     try:
-        with open(file_path, 'r', encoding='utf-8') as fs:
+        with open(file_path, 'r', encoding="utf-8") as fs:
             return json.load(fs) # channelSecret & accessToken
     except FileNotFoundError:
-        print('failed to load credential')
-        quit("the LINE bot API is terminated")
+        quit(f"failed to load credential: {file_path}")
