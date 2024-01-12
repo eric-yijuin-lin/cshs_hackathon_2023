6f14d4598d70bccd9b4ed8ef908b640050ece12d
Eric Lin <eric.yijuin.lin@gmail.com>
2024-01-12 08:57:12 +0800

    update info tech game

--- info_tech.py
+++ info_tech.py
@@ -1,4 +1,5 @@
 import random
+import time
 from datetime import datetime
 import gspread
 import utilities
@@ -6,7 +7,16 @@ import utilities
 class MathGame:
     def __init__(self, config_file: str) -> None:
         self.operators = ['+', '-', '*', '/']
+        self.game_sheet = None
+        self.log_sheet = None
+        self.last_card_update = 0
+        self.last_log_update = 0
         self.config = utilities.read_config(config_file)
+        self.init_work_sheets()
+        self.card_pool = []
+        self.pending_logs = []
+    
+    def init_work_sheets(self) -> None:
         credential_path = self.config["sheetCredentialPath"]
         book_title = self.config["workBook"]
         service_account = gspread.service_account(filename = credential_path)
@@ -44,6 +54,7 @@ class MathGame:
         return 100 * multiplier
 
     def draw_card(self, user: str) -> dict:
+        self.refresh_card_pool()
         all_cards = self.game_sheet.get_all_records()
         card = random.choice(all_cards)
         max_atk = card["最大攻擊力"]
@@ -52,11 +63,24 @@ class MathGame:
         card["防禦力"] = random.randint(max_def // 2, max_def)
         self.insert_log(user, card)
         return card
+
+    def refresh_card_pool(self) -> None:
+        ts_now = time.time()
+        if ts_now - self.last_card_update > 10:
+            self.card_pool = self.game_sheet.get_all_records()
+            self.last_card_update = time.time()
     
     def insert_log(self, user: str, card: dict) -> None:
         log_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_text = f"{user} 抽到了攻擊力 {card['攻擊力']} 防禦力 {card['防禦力']} 的 {card['卡片名稱']}"
+        self.pending_logs.append([log_time, log_text])
         try:
-            log_text = f"{user} 抽到了攻擊力 {card['攻擊力']} 防禦力 {card['防禦力']} 的 {card['卡片名稱']}"
-            self.log_sheet.insert_row([log_time, log_text], 2)
+            self.flush_log()
         except Exception as ex:
-            print("failed to inse log: ", ex)
+            print("failed to insert logs: ", ex)
+
+    def flush_log(self) -> None:
+        ts_now = time.time()
+        if ts_now - self.last_log_update > 10:
+            self.log_sheet.insert_rows(self.pending_logs, 2)
+            self.last_log_update = time.time()
